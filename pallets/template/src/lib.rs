496fd5c01d3a6e7892160bf1ef19c0776bab16fd
Andy Bell <andy.bell@displaynote.com>
2021-04-15 20:03:05 +0200

    OCW boilerplate stuff

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -6,6 +6,65 @@
 
 pub use pallet::*;
 
+use core::{convert::TryInto, fmt};
+use frame_support::{
+	debug, decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult,
+};
+use codec::{Decode, Encode};
+
+use frame_system::{
+	ensure_none, ensure_signed,
+	offchain::{
+		AppCrypto, CreateSignedTransaction, SendSignedTransaction, SendUnsignedTransaction,
+		SignedPayload, Signer, SigningTypes, SubmitTransaction,
+	},
+};
+use sp_core::crypto::KeyTypeId;
+use sp_runtime::{
+	offchain as rt_offchain,
+	offchain::{
+		storage::StorageValueRef,
+		storage_lock::{BlockAndTime, StorageLock},
+	},
+	transaction_validity::{
+		InvalidTransaction, TransactionSource, TransactionValidity, ValidTransaction,
+	},
+	RuntimeDebug,
+};
+use sp_std::{collections::vec_deque::VecDeque, prelude::*, str};
+
+use serde::{Deserialize, Deserializer};
+
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+
+/// Based on the above `KeyTypeId` we need to generate a pallet-specific crypto type wrapper.
+/// We can utilize the supported crypto kinds (`sr25519`, `ed25519` and `ecdsa`) and augment
+/// them with the pallet-specific identifier.
+pub mod crypto {
+	use crate::KEY_TYPE;
+	use sp_core::sr25519::Signature as Sr25519Signature;
+	use sp_runtime::app_crypto::{app_crypto, sr25519};
+	use sp_runtime::{traits::Verify, MultiSignature, MultiSigner};
+
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct TestAuthId;
+	// implemented for ocw-runtime
+	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+
+	// implemented for mock runtime in test
+	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
+	for TestAuthId
+	{
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+}
 #[cfg(test)]
 mod mock;
 
